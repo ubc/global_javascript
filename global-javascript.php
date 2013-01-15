60343a128d7e933e8854f9f13d307d096400bcd4
psmagicman <julienlaw1@gmail.com>
2013-01-15 12:31:28 -0800

    Changed functionality of core code to edit javscript instead of css

--- global-javascript.php
+++ global-javascript.php
@@ -3,7 +3,7 @@
 Plugin Name: Global Javascript
 Plugin URI: 
 Description: Allows the creation and editing of Javascript on Wordpress powered sites
-Version: 0.1
+Version: 0.2
 Author: Julien Law
 Author URI:
 Based on the Improved Simpler CSS plugin by CTLT
@@ -26,55 +26,55 @@ Based on the Improved Simpler CSS plugin by CTLT
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 if($_GET['code'] != 'none'):
-	add_action('admin_print_scripts-appearance_page_imporved-simpler-css/improved-simpler-css', 'improved_simpler_css_admin_print_scripts');
+	add_action('admin_print_scripts-appearance_page_global-javascript/global-javascript', 'global_javascript_admin_print_scripts');
 endif;
 
-add_action( 'admin_print_scripts-appearance_page_imporved-simpler-css/improved-simpler-css', 'improved_simpler_css_admin_enqueue_scripts');
-add_action( 'admin_print_styles-appearance_page_imporved-simpler-css/improved-simpler-css', 'improved_simpler_css_admin_print_styles');
-add_action( 'admin_init', 'improved_simpler_css_init' );
-add_action( 'wp_before_admin_bar_render', 'improved_simpler_css_admin_bar_render' );
+add_action( 'admin_print_scripts-appearance_page_global-javascript/global-javascript', 'global_javascript_admin_enqueue_scripts');
+add_action( 'admin_print_styles-appearance_page_global-javascript/global-javascript', 'global_javascript_admin_print_styles');
+add_action( 'admin_init', 'global_javascript_init' );
+add_action( 'wp_before_admin_bar_render', 'global_javascript_admin_bar_render' );
 
 
 // Override the edit link, the default link causes a redirect loop
-add_filter('get_edit_post_link', 'improved_simpler_css_revision_post_link');
+add_filter('get_edit_post_link', 'global_javascript_revision_post_link');
 
 /**
- * improved_simpler_css_admin_print_styles function.
+ * global_javascript_admin_print_styles function.
  * adds styles to the admin page
  * @access public
  * @return void
  */
-function improved_simpler_css_admin_print_styles() {
-	wp_enqueue_style('simpler-css', plugins_url('/imporved-simpler-css/css/styles.css'));
+function global_javascript_admin_print_styles() {
+	wp_enqueue_style('global-js', plugins_url('/global-javascript/css/styles.css'));
 }
 /**
- * improved_simpler_css_admin_enqueue_scripts function.
+ * global_javascript_admin_enqueue_scripts function.
  * adds the pos
  * @access public
  * @return void
  */
-function improved_simpler_css_admin_enqueue_scripts() {
+function global_javascript_admin_enqueue_scripts() {
 	// wp_enqueue_script( 'postbox' );
 }
 /**
- * improved_simpler_css_admin_print_scripts function.
+ * global_javascript_admin_print_scripts function.
  * 
  * @access public
  * @return void
  */
-function improved_simpler_css_admin_print_scripts() {
-	wp_enqueue_script('simpler-css', plugins_url('/imporved-simpler-css/js/codemirror.js'));
+function global_javascript_admin_print_scripts() {
+	wp_enqueue_script('global-js', plugins_url('/global-javascript/js/codemirror.js'));
 }
 
 
 
 /**
- * improved_simpler_css_admin_bar_render function.
+ * global_javascript_admin_bar_render function.
  * Add the menu to the admin bar
  * @access public
  * @return void
  */
-function improved_simpler_css_admin_bar_render() {
+function global_javascript_admin_bar_render() {
 	global $wp_admin_bar;
 	// we can remove a menu item, like the COMMENTS link
     // just by knowing the right $id
@@ -82,9 +82,9 @@ function improved_simpler_css_admin_bar_render() {
 	// we can add a submenu item too
 	$wp_admin_bar->add_menu( array(
         'parent' => 'appearance',
-        'id' => 'custom-css',
-        'title' => __('Custom CSS'),
-        'href' => admin_url( 'themes.php?page=imporved-simpler-css/improved-simpler-css.php')
+        'id' => 'custom-js',
+        'title' => __('Custom JS'),
+        'href' => admin_url( 'themes.php?page=global-javascript/global-javascript.php')
     ) );
 }
 
@@ -92,19 +92,19 @@ function improved_simpler_css_admin_bar_render() {
 
 
 /**
- * improved_simpler_css_revision_post_link function.
+ * global_javascript_revision_post_link function.
  * Override the edit link, the default link causes a redirect loop
  * @access public
  * @param mixed $post_link
  * @return void
  */
-function improved_simpler_css_revision_post_link($post_link)
+function global_javascript_revision_post_link($post_link)
 {
 	global $post;
 	
-	if ( isset( $post ) && ( 's-custom-css' == $post->post_type ) )
+	if ( isset( $post ) && ( 's-custom-js' == $post->post_type ) )
 		if ( strstr( $post_link, 'action=edit' ) && !strstr( $post_link, 'revision=' ) )
-			$post_link = 'themes.php?page=imporved-simpler-css/improved-simpler-css.php';
+			$post_link = 'themes.php?page=global-javascript/global-javascript.php';
 	
 	return $post_link;
 
@@ -112,14 +112,14 @@ function improved_simpler_css_revision_post_link($post_link)
 
 
 /**
- * improved_simpler_css_init function.
+ * global_javascript_init function.
  * Init plugin options to white list our options
  * @access public
  * @return void
  */
-function improved_simpler_css_init(){
+function global_javascript_init(){
 	/*
-	register_setting( 'simpler_css_options', 'simpler_css_css');
+	register_setting( 'global_js_options', 'global_js_js');
 	*/
 	$args = array(
 	    'public' => false,
@@ -127,39 +127,39 @@ function improved_simpler_css_init(){
 	    'capability_type' => 'nav_menu_item',
 	    'supports' 		=> array('revisions')
   	); 
- 	register_post_type( 's-custom-css', array(
+ 	register_post_type( 's-custom-js', array(
 		'supports' => array( 'revisions' )
 	) );
 	
 }
 
 /**
- * improved_simpler_css_save_revision function.
+ * global_javascript_save_revision function.
  * safe the revisoin 
  * @access public
  * @param mixed $css
  * @return void
  */
-function improved_simpler_css_save_revision( $css ) {
+function global_javascript_save_revision( $js ) {
 
 	// If null, there was no original safecss record, so create one
-	if ( !$safecss_post = improved_simpler_css_get_css() ) {
+	if ( !$safejs_post = global_javascript_get_js() ) {
 		$post = array();
-		$post['post_content'] = $css;
-		$post['post_title']   = 'Custom CSS';
+		$post['post_content'] = $js;
+		$post['post_title']   = 'Custom JS';
 		$post['post_status']  = 'publish';
-		$post['post_type']    = 's-custom-css';
+		$post['post_type']    = 's-custom-js';
 		
 		// check if there are any settings data 
-		$simpler_css_css = get_option ( 'simpler_css_css' );
-		if($simpler_css_css): // option settings exist 
-			if(!is_array($simpler_css_css))
-				$simpler_css_css = array($simpler_css_css);
+		$global_js_js = get_option ( 'global_js_js' );
+		if($global_js_js): // option settings exist 
+			if(!is_array($global_js_js))
+				$global_js_js = array($global_js_js);
 			
-			array_reverse($simpler_css_css);
+			array_reverse($global_js_js);
 			$count = 0;
-			foreach($simpler_css_css  as $css_from_option):
-				$post['post_content'] = $css_from_option;
+			foreach($global_js_js  as $js_from_option):
+				$post['post_content'] = $js_from_option;
 				if($count == 0):
 					$post_id = wp_insert_post( $post );
 				else:	
@@ -172,11 +172,11 @@ function improved_simpler_css_save_revision( $css ) {
 			endforeach;
 			
 			// ok lets update the post for real this time
-			$post['post_content'] = $css; // really update the stuff
+			$post['post_content'] = $js; // really update the stuff
 			wp_update_post( $post );
 			
 			// time to delete the options that exits
-			delete_option('simpler_css_css');
+			delete_option('global_js_js');
 			
 		else: // there is no settins data lets save this stuff
 			$post_id = wp_insert_post( $post );
@@ -185,47 +185,47 @@ function improved_simpler_css_save_revision( $css ) {
 		return true;
 	} // there is a styles store in the custom post type
 	
-	$safecss_post['post_content'] = $css;
+	$safejs_post['post_content'] = $js;
 	
-	wp_update_post( $safecss_post );
+	wp_update_post( $safejs_post );
 	return true;
 }
 /**
- * improved_simpler_css_get_css function.
- * Get the custom css from posts table 
+ * global_javascript_get_js function.
+ * Get the custom js from posts table 
  * @access public
  * @return void
  */
-function improved_simpler_css_get_css()
+function global_javascript_get_js()
 {
-	if ( $a = array_shift( get_posts( array( 'numberposts' => 1, 'post_type' => 's-custom-css', 'post_status' => 'publish' ) ) ) )
-		$safecss_post = get_object_vars( $a );
+	if ( $a = array_shift( get_posts( array( 'numberposts' => 1, 'post_type' => 's-custom-js', 'post_status' => 'publish' ) ) ) )
+		$safejs_post = get_object_vars( $a );
 	else
-		$safecss_post = false;
+		$safejs_post = false;
 
-	return $safecss_post;
+	return $safejs_post;
 }
 /**
- * improved_simpler_css_always_get_css function.
+ * global_javascript_always_get_js function.
  * return the 
  * @access public
  * @return void
  */
-function improved_simpler_css_always_get_css()
+function global_javascript_always_get_js()
 {
 	
-	if ( $a = array_shift( get_posts( array( 'numberposts' => 1, 'post_type' => 's-custom-css', 'post_status' => 'publish' ) ) ) ):
-		$safecss_post = get_object_vars( $a );
-		return $safecss_post['post_content'];
+	if ( $a = array_shift( get_posts( array( 'numberposts' => 1, 'post_type' => 's-custom-js', 'post_status' => 'publish' ) ) ) ):
+		$safejs_post = get_object_vars( $a );
+		return $safejs_post['post_content'];
 	// if there is no 
 	else:
 	
-		$simpler_css_css = get_option ( 'simpler_css_css' );
-		if(!empty($simpler_css_css)):
-			if(!is_array($simpler_css_css))
-				$simpler_css_css = array($simpler_css_css);
+		$global_js_js = get_option ( 'global_js_js' );
+		if(!empty($global_js_js)):
+			if(!is_array($global_js_js))
+				$global_js_js = array($global_js_js);
 		
-			return $simpler_css_css[0];
+			return $global_js_js[0];
 		else:
 			// return an empty string 
 			return false;
@@ -236,39 +236,39 @@ function improved_simpler_css_always_get_css()
 
 
 
-function improved_simpler_css_addcss() {
-	$simpler_css_css = get_option ( 'simpler_css_css' );
-	if(!is_array($simpler_css_css))
-		$simpler_css_css = array($simpler_css_css);
+function global_javascript_addjs() {
+	$global_js_js = get_option ( 'global_js_js' );
+	if(!is_array($global_js_js))
+		$global_js_js = array($global_js_js);
 		
-	echo '<style type="text/css">' . "\n";
-	echo improved_simpler_css_filter ( improved_simpler_css_always_get_css() ) . "\n";
-	echo '</style>' . "\n";
+	echo '<script type="text/javascript">' . "\n";
+	echo global_javascript_filter ( global_javascript_always_get_js() ) . "\n";
+	echo '</script>' . "\n";
 }
 
-function improved_simpler_css_filter($_content) {
+function global_javascript_filter($_content) {
 	$_return = preg_replace ( '/@import.+;( |)|((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/))/i', '', $_content );
 	$_return = htmlspecialchars ( strip_tags($_return), ENT_NOQUOTES, 'UTF-8' );
 	return $_return;
 }
 
-function improved_simpler_css_menu() {
-	add_theme_page ( 'Custom CSS', 'Custom CSS', 8, __FILE__, 'improved_simpler_css_options' );
+function global_javascript_menu() {
+	add_theme_page ( 'Custom JS', 'Custom JS', 8, __FILE__, 'global_javascript_options' );
 }
 
-function improved_simpler_css_options() {
+function global_javascript_options() {
 	
-$simpler_css_default = '/* Welcome to Improved Simpler CSS!
+$global_js_default = '/* Welcome to Global Javascript!
 
-If you are familiar with CSS, you may delete these comments and get started. CSS (Cascading Style Sheets) is a kind of code that tells the browser how to render a web page. Here\'s an example:
+If you are familiar with Javascript, you may delete these comments and get started. Javascript allows websites to have dynamic content. Here\'s an example:
 
-img { border: 1px dotted red; }
+alert("Hello");
 
-That line basically means "give images a dotted red border one pixel thick."
+That line will display a popup message box that says Hello
 
-CSS is not very hard to learn. There are many free references to help you get started, like http://www.w3schools.com/css/default.asp
+Javascript is not very hard to learn. There are many free references to help you get started, like http://www.w3schools.com/js/default.asp
 
-We hope you enjoy developing your custom CSS. Here are a few things to keep in mind:
+We hope you enjoy developing your custom JS. Here are a few things to keep in mind:
  - You cannot edit the stylesheets of your theme. Your stylesheet will be loaded after the theme stylesheets, which means that your rules can take precedence and override the theme CSS rules.
  - CSS comments will be stripped from your stylesheet when outputted. */
 
@@ -289,22 +289,22 @@ Things we encourage include:
 
 
 	$updated = false;
-	$opt_name = 'simpler_css_css';
+	$opt_name = 'global_js_js';
 
 	
-	$css_val = improved_simpler_css_always_get_css();
-	if (!$css_val)
-		$css_val = array($simpler_css_default);
-	elseif(!is_array($css_val))
-		$css_val = array($css_val);
+	$js_val = global_javascript_always_get_js();
+	if (!$js_val)
+		$js_val = array($global_js_default);
+	elseif(!is_array($js_val))
+		$js_val = array($js_val);
 	
 	// the form has been submited save the options 
-	if (!empty($_POST) && check_admin_referer('update_simpler_css_css','update_simpler_css_css_field') ):
+	if (!empty($_POST) && check_admin_referer('update_global_js_js','update_global_js_js_field') ):
 		
 		
-		$css_form = stripslashes ( $_POST [$opt_name] );
-		improved_simpler_css_save_revision( $css_form );
-		$css_val[0] = $css_form;
+		$js_form = stripslashes ( $_POST [$opt_name] );
+		global_javascript_save_revision( $js_form );
+		$js_val[0] = $js_form;
 		$updated = true;
 		$message_number = 1; 
 		?>
@@ -316,39 +316,39 @@ Things we encourage include:
 			
 	if($message_number):
 		
-		$messages['s-custom-css'] = array(
-		 1 => "Custom CSS Saved",
-		 5 => isset($_GET['revision']) ? sprintf( __('Custom CSS restored to revision from %s, <em>Save Changes for the revision to take effect</em>'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false
+		$messages['s-custom-js'] = array(
+		 1 => "Custom JS Saved",
+		 5 => isset($_GET['revision']) ? sprintf( __('Custom JS restored to revision from %s, <em>Save Changes for the revision to take effect</em>'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false
 		 );
 		 $messages = apply_filters( 'post_updated_messages', $messages );
 		 ?>
-		<div class="updated"><p><strong><?php echo $messages['s-custom-css'][$message_number]; ?></strong></p></div>		
+		<div class="updated"><p><strong><?php echo $messages['s-custom-js'][$message_number]; ?></strong></p></div>		
 	<?php endif ?>
 
 
 <div class="wrap">
 <div class="icon32" id="icon-themes"><br></div>
-<h2>Custom CSS</h2>
+<h2>Custom JS</h2>
 <div id="poststuff" class="metabox-holder<?php echo 2 == $screen_layout_columns ? ' has-right-sidebar' : ''; ?>">
 <ul class="subsubsub">
 	<?php if($_GET['code'] !="none" ): ?>
-	<li ><strong>Advanced Editor</strong> switch to: <a  href="?page=imporved-simpler-css/improved-simpler-css.php&code=none">simpler</a></li>
+	<li ><strong>Advanced Editor</strong> switch to: <a  href="?page=global-javascript/global-javascript.php&code=none">simpler</a></li>
 	<?php else: ?>
-	<li ><strong>Simple Editor</strong> switch to: <a  href="?page=imporved-simpler-css/improved-simpler-css.php">advance</a></li>
+	<li ><strong>Simple Editor</strong> switch to: <a  href="?page=global-javascript/global-javascript.php">advance</a></li>
 	<?php endif; ?>
 </ul>
 <div id="code-version-toggle">
-<h3 style="clear:both;">Edit CSS</h3>
+<h3 style="clear:both;">Edit JS</h3>
 	
 	<?php if($_GET['code'] !="none"): ?>
 		<p class="search-box"><input type='text' style="width: 15em" id='query' value=''> <input type="button" onclick="search()" value="Search" class="button">  and replace with <input type='text' style="width: 15em" id="replace">
 		<input onclick="replace1();" type="button" class="button" value="Replace All"> 
 		</p>
 	<?php endif; ?>
-	<form method="post" action="themes.php?page=imporved-simpler-css/improved-simpler-css.php<?php if($_GET['code'] == "none") {echo "&code=none"; } ?>">
-		<?php settings_fields('simpler_css_options'); ?>
-		<?php wp_nonce_field( 'update_simpler_css_css','update_simpler_css_css_field' ); ?>
-		<textarea cols="80" rows="25" id="simpler_css_css" name="<?php echo $opt_name; ?>"><?php echo $css_val[0]; ?></textarea>
+	<form method="post" action="themes.php?page=global-javascript/global-javascript.php<?php if($_GET['code'] == "none") {echo "&code=none"; } ?>">
+		<?php settings_fields('global_js_options'); ?>
+		<?php wp_nonce_field( 'update_global_js_js','update_global_js_js_field' ); ?>
+		<textarea cols="80" rows="25" id="global_js_js" name="<?php echo $opt_name; ?>"><?php echo $js_val[0]; ?></textarea>
 		<input type="hidden" name="action" value="update" /> <input type="hidden" name="page_options" value="<?php echo $opt_name?>" />
 		<p class="submit"><input type="submit" class="button-primary" value="<?php _e ( 'Save Changes' )?>" /> <span id="unsaved_changes" <?php if(!isset($_GET['revision'])) { ?>style="display:none;"<?php } ?> >There are some unsaved changes</span> </p>
 	</form>
@@ -356,12 +356,12 @@ Things we encourage include:
 
 <?php if($_GET['code'] !="none"): ?>
 <script>
-	var css_change = 0;
-	var editor = CodeMirror.fromTextArea(document.getElementById("simpler_css_css"), {
-		mode: "text/css", lineNumbers: true, indentUnit:2,
+	var js_change = 0;
+	var editor = CodeMirror.fromTextArea(document.getElementById("global_js_js"), {
+		mode: "text/javascript", lineNumbers: true, indentUnit:2,
 		onChange: function(){
-			css_change++;
-			if(css_change > 1)
+			js_change++;
+			if(js_change > 1)
 			{
 			 jQuery("#unsaved_changes").show();
 			
@@ -405,28 +405,28 @@ Things we encourage include:
 
 <?php endif; 
 
-$safecss_post = improved_simpler_css_get_css();
+$safejs_post = global_javascript_get_js();
 
-if ( 0 < $safecss_post['ID'] && wp_get_post_revisions( $safecss_post['ID'] ) ) {
-	function post_revisions_meta_box( $safecss_post ) {
+if ( 0 < $safejs_post['ID'] && wp_get_post_revisions( $safejs_post['ID'] ) ) {
+	function post_revisions_meta_box( $safejs_post ) {
 		// Specify numberposts and ordering args
 		$args = array( 'numberposts' => 5, 'orderby' => 'ID', 'order' => 'DESC' );
 		// Remove numberposts from args if show_all_rev is specified
 		if ( isset( $_GET['show_all_rev'] ) )
 			unset( $args['numberposts'] );
 
-		wp_list_post_revisions( $safecss_post['ID'], $args );
+		wp_list_post_revisions( $safejs_post['ID'], $args );
 	}
 
-	add_meta_box( 'revisionsdiv', __( 'CSS Revisions', 'safecss' ), 'post_revisions_meta_box', 's-custom-css', 'normal' );
-	do_meta_boxes( 's-custom-css', 'normal', $safecss_post );
+	add_meta_box( 'revisionsdiv', __( 'JS Revisions', 'safejs' ), 'post_revisions_meta_box', 's-custom-js', 'normal' );
+	do_meta_boxes( 's-custom-js', 'normal', $safejs_post );
 }
 ?>
 </div></div>
 <?php 
 }
 
-add_action ( 'admin_menu', 'improved_simpler_css_menu' );
-add_action ( 'wp_head', 'improved_simpler_css_addcss' );
+add_action ( 'admin_menu', 'global_javascript_menu' );
+add_action ( 'wp_head', 'global_javascript_addjs' );
 
 
