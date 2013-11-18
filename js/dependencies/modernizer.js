0f013a28b5997cbb7b46facf77c7a6518fa364ed
Enej Bajgoric <enej.bajgoric@gmail.com>
2013-11-18 11:36:29 -0800

    Updated modernizer to 2.7

--- js/dependencies/modernizer.js
+++ js/dependencies/modernizer.js
@@ -1,4 +1,4 @@
-/* Modernizr 2.6.2 (Custom Build) | MIT & BSD
+/* Modernizr 2.7.0 (Custom Build) | MIT & BSD
  * Build: http://modernizr.com/download/#-fontface-backgroundsize-borderimage-borderradius-boxshadow-flexbox-flexboxlegacy-hsla-multiplebgs-opacity-rgba-textshadow-cssanimations-csscolumns-generatedcontent-cssgradients-cssreflections-csstransforms-csstransforms3d-csstransitions-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websockets-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-cssclasses-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-load
  */
 ;
@@ -7,7 +7,7 @@
 
 window.Modernizr = (function( window, document, undefined ) {
 
-    var version = '2.6.2',
+    var version = '2.7.0',
 
     Modernizr = {},
 
